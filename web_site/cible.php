fbd68952430f2b725e16d0a6a90920a66f68bb55
Grégoire Lefaure <gregoire@lefaure.fr>
2020-07-09 20:33:16 +0200

    Site web mis en place

--- web_site/cible.php
+++ web_site/cible.php
@@ -1,5 +1,7 @@
 <?php
-$bdd = new PDO('mysql:host=127.0.0.1;dbname=elevage', 'greg', 'MgfssQWERTY eia24a.' array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-$requete = $bdd->prepare('INSERT INTO nimal(nom, possesseur) VALUES(?, ?)');
-$requete->execute(array($_POST['nom'], $_POST['possesseur']));
+$bdd = new PDO('mysql:host=localhost;dbname=weekly_mail', 'weekly_mail', 'Mgfa23a.', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$requete = $bdd->prepare('INSERT INTO Users (name, surname, gender, birthday, email) VALUES(?, ?, ?, ?, ?)');
+$requete->execute(array($_POST['name'], $_POST['surname'], $_POST['gender'], $_POST['birthday'], $_POST['email']));
+header('Location: thanks.html');
+exit();
 ?>
